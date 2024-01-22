9b2b0e95fb0d5cd39e5d00a67a246de80501c40a
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-01-21 22:59:55 -0500

    post_created event received by posts service

--- events/main_test.go
+++ events/main_test.go
@@ -8,13 +8,13 @@ import (
 func TestFindEvent(t *testing.T) {
 	body := []byte(`{"type": "comment_created", "data": {"id": "1", "post_id": "1", "content": "hello"}}`)
 	expected := "comment_created"
-	reflect.DeepEqual(FindEventType(body), expected)
+	reflect.DeepEqual(findEventType(body), expected)
 }
 
 func TestFindEvent2(t *testing.T) {
 	body := []byte(`{"type": "post_created", "data": {"id": "1", "title": "hello"}}`)
 	expected := "post_created"
-	reflect.DeepEqual(FindEventType(body), expected)
+	reflect.DeepEqual(findEventType(body), expected)
 }
 
 func TestFindEventDomain(t *testing.T) {
--- posts/main.go
+++ posts/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/google/uuid"
 )
+
 type Post struct {
 	ID string `json:"id"`
 	Title string `json:"title"`
@@ -33,17 +39,69 @@ func post(c *fiber.Ctx) error {
 		Message: "Post created successfully",
 		Data: *p,
 	}
+	publishEvent("post_created", p)
 	return c.JSON(res)
 }
 
+func receiveEvent(c *fiber.Ctx) error {
+	fmt.Printf("Received event: %s\n", c.Body())
+	return nil
+}
+
+func subscribeToEvents(events []string) error {
+	for _, event := range events {
+		agent := fiber.Post("http://localhost:8080/subscribe")
+		body := fiber.Map{
+			"host": "http://localhost:8081",
+			"event_type": event,
+		}
+
+		agent.JSON(body)
+		_, _, errs := agent.Bytes()
+
+		if errs != nil {
+			return errs[0]
+		}
+	}
+	return nil;
+}
+
+func publishEvent(eventType string, data interface{}) error {
+	agent := fiber.Post("http://localhost:8080/publish")
+	body := fiber.Map{
+		"type": eventType,
+		"data": data,
+	}
+
+	agent.JSON(body)
+	_, _, errs := agent.Bytes()
+
+	if errs != nil {
+		return errs[0]
+	}
+	return nil;
+}
+
 func main() {
+	// subscribe to events
+	err := subscribeToEvents([]string{"post_created"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	app := fiber.New()
 
+	// middleware
+	app.Use(logger.New())
+	app.Use(cors.New())
+
+	// routes
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.JSON(posts)
 	})
 
 	app.Post("/", post)
+	app.Post("/events", receiveEvent)
 
 	app.Listen(":8081")
 }
