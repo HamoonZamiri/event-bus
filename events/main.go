22ab42de22c2cc085b1446742d2b6de75f41e759
Hamoon Zamiri <hamoon.zamiri@mail.utoronto.ca>
2024-01-29 22:41:42 -0500

    feat: add docker compose to run event bus and redis server

--- events/main.go
+++ events/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+
 	"example/event-bus/controller"
 	"example/event-bus/router"
 	"example/event-bus/service"
@@ -8,12 +10,15 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+var ctx = context.Background()
+
 func main() {
 	app := router.New()
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World 👋!")
 	})
 
+	// redisStore := service.NewRedisClient(ctx)
 	eventStore := service.NewEventStore([]string{"comment_created", "post_created", "comment_moderated"})
 	c := controller.NewController(eventStore)
 	c.Register(app)
